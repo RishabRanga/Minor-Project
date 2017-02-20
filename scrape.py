726bcba656ede506f52ea4db8488fe277f8d1dc7
RishabRanga <rishab.ranga1996@gmail.com>
2017-02-20 13:11:47 +0530

    scaped data with replies

--- scrape.py
+++ scrape.py
@@ -1,23 +1,32 @@
 import praw
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 #install twython and from nltk.download() get vader_lexicon in models
-
-reddit = praw.Reddit(user_agent='Comment Extraction', client_id='CLIENT_ID', client_secret='CLIENT_SECRET', username='USERNAME', password='PASSWORD')
+reddit = praw.Reddit(user_agent='Comment Extraction (by /u/paddy1709)', client_id='ZBfMvW4KD7Yr2A', client_secret='KVmYPP5cTUVmubAVaN5Eizbuo14', username='paddy1709', password='5368745')
 
 submission = reddit.submission(url='https://www.reddit.com/r/SquaredCircle/comments/5si8pp/live_raw_discussion_thread_feb_6th_2017/')
-
+submission.comments.replace_more(limit=100)
+#flat_comments = praw.helpers.flatten_tree(submission.comments)
 sid = SentimentIntensityAnalyzer()
-
+#submission.comments.replace_more(limit=0)
 c=1
 f=open('Roman.txt','w')
 f2=open('Braun.txt','w')
 k=1
-for comment in submission.comments.list():
+for comment in submission.comments:
     	#print(comment.body)
-    	print comment.body
-    	print k
-    	k+=1
-	for i in comment.body.split(' '):
+    print("score for the below comment is"+str(comment.score))
+    print comment.body
+        
+    print k
+    k+=1
+    #commentSubmission = reddit.submission(url="https://www.reddit.com"+comment.permalink(fast=True))
+    print("https://www.reddit.com"+comment.permalink(fast=True))
+    print("replies for the comment")
+    for repy in comment.replies:
+        print(repy.body)
+    #print(commentSubmission.comments[1])
+    for i in comment.body.split(' '):
 	    	ss=sid.polarity_scores(comment.body)
 	    	if i=='Roman':	
 	    		f.write(comment.body)
@@ -36,7 +45,7 @@ for comment in submission.comments.list():
 	    		f2.write('\n')
 	    		c=c+1
 	
-print c
+#print c
 	   
 """
 Arranges comments based on highest points and prints it to console
